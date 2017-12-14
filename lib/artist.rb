29f67ed2d3fd2872d35c64b431d0cbd6abb369f6
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 18:46:13 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,8 +16,8 @@ song.artist = self
 end
 
 
-def add_song_by_name(name, genre)
-song = Song.new(name, genre)
+def add_song_by_name(name)
+song = Song.new(name)
 @songs << song
 song.artist = self
  @@song_count +=1 # for every new song the count goes up by 1
