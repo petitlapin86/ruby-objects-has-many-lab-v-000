d5a6ab8eb7e2b3ddbce65ddc79484a897f029a05
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 18:48:10 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author
 attr_accessor :name
 
-@@post_count = 0 #post count starts at zero 
+@@post_count = 0 #post count starts at zero
 
 def initialize(name)
 @name = name
@@ -29,5 +29,5 @@ end
 def self.post_count
    @@post_count # keeps track of number of posts
  end
- 
+
 end
