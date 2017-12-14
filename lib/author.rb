eea4f6339a67ccf0fe3f8da7224bfa2f8c2ce657
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 18:37:02 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,24 @@
+class Author
+attr_accessor :name
+
+def initialize(name)
+@name = name
+@songs = []
+end
+
+def add_song(song)
+@songs << song
+song.artist = self
+end
+
+
+def add_song_by_name(name, genre)
+song = Song.new(name, genre)
+@songs << song
+song.artist = self
+end
+
+def songs
+@songs
+end
+end
