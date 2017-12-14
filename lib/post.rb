0e0954599bdb634e6aebfbe488869d6d2cf7eb6d
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 18:40:03 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,15 @@
+class Post
+  attr_accessor :name, :author #belongs to an artist
+
+  def initialize(name) #initialize the song class with a name
+  @name = name
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+  else
+    nil #we dont want code to break if theres no artist, so we ask it to return nil in this case
+  end
+end
+end
