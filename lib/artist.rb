d54057db3165f6effbb85275f8cbfa6ee6920c03
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 18:44:39 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,9 @@
 class Artist
 attr_accessor :name
 
+
+  @@song_count = 0 #list of song count starts at zero 
+
 def initialize(name)
 @name = name
 @songs = []
@@ -9,6 +12,7 @@ end
 def add_song(song)
 @songs << song
 song.artist = self
+  @@song_count +=1 # for every new song the count goes up by 1
 end
 
 
