773404a8f587ab0071310765a682789e756b4d0a
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-14 18:48:02 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,8 @@
 class Author
 attr_accessor :name
 
+@@post_count = 0 #post count starts at zero 
+
 def initialize(name)
 @name = name
 @posts = []
@@ -9,6 +11,7 @@ end
 def add_post(post)
 @post << post
 post.author = self
+@@post_count +=1 # for every new post the count goes up by 1
 end
 
 
@@ -16,9 +19,15 @@ def add_post_by_title(title)
 song = Post.new(title)
 @post << post
 post.author = self
+@@post_count +=1 # for every new post the count goes up by 1
 end
 
 def post
 @post
 end
+
+def self.post_count
+   @@post_count # keeps track of number of posts
+ end
+ 
 end
